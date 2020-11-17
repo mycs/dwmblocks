3606d3139569870a27b5f7b34f4ebda926b357f9
Michael Y. C. Shih <i420cc@gmail.com>
2020-11-17 08:03:05 +0800

    auto push

--- config.h
+++ config.h
@@ -4,8 +4,8 @@ static const Block blocks[] = {
 	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -14,16 +14,16 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
-	{"",	"mailbox",	180,	12},
+	/* {"",	"memory",	10,	14}, */
+	/* {"",	"internet",	5,	4}, */
 	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
+	{"",	"forecast",	18000,	5},
+	/* {"",	"mailbox",	180,	12}, */
+	{"",	"moonphase",	18000,	17},
 	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
 	{"",	"help-icon",	0,	15},
 };
 
